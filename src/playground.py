9c3e8a0ebf8dca3e7efe93fd90ff9039f92db1d5
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2025-03-14 17:08:07 +0100

    Minor fixes

--- src/playground.py
+++ src/playground.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+import wandb
 from smolagents import LiteLLMModel, ToolCallingAgent
 from smolagents.monitoring import LogLevel
 from tools.bash import get_bash_tool
@@ -14,70 +15,73 @@ dotenv.load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")  # Openrouter models need their OPENROUTER API key
 wandb_key = os.getenv("WANDB_API_KEY")
 
-agent_id = create_new_user_and_rundir()
-tools = [get_bash_tool(agent_id, 60 * 5)]
-
-config = {
-    "agent_id" : agent_id,
-    "model" : MODELS.GPT4o,
-    "temperature" : 1,
-    "max_steps" : 30,
-    "dataset" : "human_non_tata_promoters",
-    "tags" : ["exp_prompt_dimos"],
-    "tools" : [{tool.name} for tool in tools],
-    "tools_args" : [{tool.name, str(tool.args)} for tool in tools if hasattr(tool, "args")],
-    "prompt" : "dimos_prompt_1.yaml",
-    "planning_interval" : None,
-}
-
-setup_logging(config, api_key=wandb_key)
-
-model = LiteLLMModel(
-    model_id=config['model'],
-    api_key=api_key,
-    temperature=config["temperature"],
-)
-
-agent = ToolCallingAgent(
-    tools=tools,
-    model=model,
-    add_base_tools=False,
-    max_steps=config["max_steps"],
-    prompt_templates=load_prompts(config["prompt"]),
-    planning_interval=config["planning_interval"],
-    verbosity_level=LogLevel.DEBUG,
-)
-
-user_prompt = f"""
-You are using a linux system.
-You have access to CPU only, no GPU.
-You can only work in /workspace/runs/{config['agent_id']} directory.
-Create a new conda environment called {config['agent_id']}_env (run the installation in non-verbose mode). 
-Use this environment to install any packages you need (use non-verbose mode for installations).
-Write all your python scripts in files. 
-Run all scripts in this environment.
-Your final output should be inference.py script, located in the workspace folder (/workspace/runs/{config['agent_id']}/inference.py). 
-This script will be separated from the training script and only load the already trained model and run inference.
-Any path inside the inference.py script should be absolute (eg. /workspace/runs/{config['agent_id']}/model.pth).
-The script will be taking the following named arguments:
-    --input (an input file path, file is of the same format as your training data (except the label column))
-    --output (the output file path, this file should be a one column csv file with the predictions, the column name should be 'prediction')
-
-Create a prototype model, with only few training steps, no need to optimize it for performance.
-
-Look into the workspace/datasets/{config['dataset']} folder and create a ML classifier using files in there.
-Use the ..._train.csv as training data.
-Use the ..._test.csv as testing data.
-Run all bash commands and python command in a way that prints the least possible amount of tokens into the console.
-
-Column descriptions:
-sequence: DNA sequence to be classified, alphabet: A, C, G, T, N
-Make sure to tokenize for all characters from the alphabet.
-
-class: 1 if the promoter is a non-TATA promoter, 0 otherwise
-"""
-
 for i in range(10):
+
+    agent_id = create_new_user_and_rundir()
+    tools = [get_bash_tool(agent_id, 60 * 5)]
+
+    config = {
+        "agent_id" : agent_id,
+        "model" : MODELS.GPT4o,
+        "temperature" : 1,
+        "max_steps" : 30,
+        "dataset" : "human_non_tata_promoters",
+        "tags" : ["exp_prompt_dimos"],
+        "tools" : [{tool.name} for tool in tools],
+        "tools_args" : [{tool.name, str(tool.args)} for tool in tools if hasattr(tool, "args")],
+        "prompt" : "dimos_prompt_1.yaml",
+        "planning_interval" : None,
+    }
+
+    setup_logging(config, api_key=wandb_key)
+
+    model = LiteLLMModel(
+        model_id=config['model'],
+        api_key=api_key,
+        temperature=config["temperature"],
+    )
+
+    agent = ToolCallingAgent(
+        tools=tools,
+        model=model,
+        add_base_tools=False,
+        max_steps=config["max_steps"],
+        prompt_templates=load_prompts(config["prompt"]),
+        planning_interval=config["planning_interval"],
+        verbosity_level=LogLevel.DEBUG,
+    )
+
+    user_prompt = f"""
+    You are using a linux system.
+    You have access to CPU only, no GPU.
+    You can only work in /workspace/runs/{config['agent_id']} directory.
+    Create a new conda environment called {config['agent_id']}_env (run the installation in non-verbose mode). 
+    Use this environment to install any packages you need (use non-verbose mode for installations).
+    Write all your python scripts in files. 
+    Run all scripts in this environment.
+    Your final output should be inference.py script, located in the workspace folder (/workspace/runs/{config['agent_id']}/inference.py). 
+    This script will be separated from the training script and only load the already trained model and run inference.
+    Any path inside the inference.py script should be absolute (eg. /workspace/runs/{config['agent_id']}/model.pth).
+    The script will be taking the following named arguments:
+        --input (an input file path, file is of the same format as your training data (except the label column))
+        --output (the output file path, this file should be a one column csv file with the predictions, the column name should be 'prediction')
+
+    Create a prototype model, with only few training steps, no need to optimize it for performance.
+
+    Look into the workspace/datasets/{config['dataset']} folder and create a ML classifier using files in there.
+    Use the ..._train.csv as training data.
+    Use the ..._test.csv as testing data.
+    Run all bash commands and python command in a way that prints the least possible amount of tokens into the console.
+
+    Column descriptions:
+    sequence: DNA sequence to be classified, alphabet: A, C, G, T, N
+    Make sure to tokenize for all characters from the alphabet.
+
+    class: 1 if the promoter is a non-TATA promoter, 0 otherwise
+    """
+
     agent.run(user_prompt)
     evaluate_log_run(config)
     replay(agent)
+
+    wandb.finish()
